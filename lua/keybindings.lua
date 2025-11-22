b0ba3e760b322cd1ff77580ca4fcbc77a8a23b07
vkomodey <vkomodey@gmail.com>
2025-11-21 22:33:38 -0600

    added keybindings

--- lua/keybindings.lua
+++ lua/keybindings.lua
@@ -1,1 +1,12 @@
+vim.keymap.set('n', '<Esc>', "<cmd>nohlsearch<CR>")
+vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
+vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
+vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
+vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
 
+vim.keymap.set('n', '<C-Space>', 'viw')
+vim.keymap.set('n', '  ', '<cmd>w<CR>')
+vim.keymap.set('n', '<2-LeftMouse>', '*')
+
+vim.keymap.set('n', '<leader>,', '<cmd>-tabnext<CR>')
+vim.keymap.set('n', '<leader>.', '<cmd>+tabnext<CR>')
